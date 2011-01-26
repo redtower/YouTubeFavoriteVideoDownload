0b5deca3b0259b59b26bfc08bc86b5a77adb41fc
redtower <redtower@gmail.com>
2011-01-27 00:06:17 +0900

    ipod用にm4vに変換する処理を追加

--- ytfd.sh
+++ ytfd.sh
@@ -31,11 +31,23 @@ do
     fi
 done
 
+for i in *.mp4
+do
+    if [ "$i" != "*.flv" ]; then
+        t=`echo $i | sed -e "s/\.mp4/\.m4v/"`
+        ffmpeg -i "$i" -f ipod -vcodec mpeg4 -b 1200k -mbd 2 -flags mv4+aic -trellis 2 -cmp 2 -subcmp 2 -s 320x180 -r 30000/1001 -acodec libfaac -ar 44100 -ab 128k "$t" > /dev/null 2> /dev/null
+
+        unlink $i
+    fi
+done
+
 for i in *.flv
 do
     if [ "$i" != "*.flv" ]; then
         t=`echo $i | sed -e "s/\.flv/\.mp4/"`
-        ffmpeg -i $i -vcodec copy -acodec copy $t > /dev/null 2> /dev/null
+        ffmpeg -i "$i" -vcodec copy -acodec copy "$t" > /dev/null 2> /dev/null
+        t=`echo $i | sed -e "s/\.flv/\.m4v/"`
+        ffmpeg -i "$i" -f ipod -vcodec mpeg4 -b 1200k -mbd 2 -flags mv4+aic -trellis 2 -cmp 2 -subcmp 2 -s 320x180 -r 30000/1001 -acodec libfaac -ar 44100 -ab 128k "$t" > /dev/null 2> /dev/null
 
         unlink $i
     fi
@@ -45,6 +57,14 @@ if [ "" != "$opt_outdir" ]; then
     for i in *.mp4
     do
         if [ "$i" = "*.mp4" ]; then
+            exit;
+        fi
+        mv $i $opt_outdir/
+    done
+
+    for i in *.m4v
+    do
+        if [ "$i" = "*.m4v" ]; then
             exit;
         fi
         mv $i $opt_outdir/
